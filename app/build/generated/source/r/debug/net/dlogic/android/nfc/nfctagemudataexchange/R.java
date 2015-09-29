8c24b54b50246b829b0087ab86c598bd8d90aa5c
developer4 <support@d-logic.rs>
2015-09-29 07:17:20 +0000

    U međuvremenu je Aca Marković uradio deo sa tajmerom i proširenje. Takođe je izmenjen odziv uFR-a u Card Emulation modu - sada je dvobajtni umesto pređašnjeg jednobajtnog. Nije rađen svn komit pa to radim ovom prilikom.
    git-svn-id: svn://oldsvnserver/sw_open/ufr/android/nfc_data_exchange_through_card_emulation/trunk@6 89a52e05-b780-471e-aca5-7987a5cc1b8f

--- app/build/generated/source/r/debug/net/dlogic/android/nfc/nfctagemudataexchange/R.java
+++ app/build/generated/source/r/debug/net/dlogic/android/nfc/nfctagemudataexchange/R.java
@@ -17,17 +17,16 @@ public final class R {
     public static final class id {
         public static final int LinearLayout01=0x7f090001;
         public static final int ScrollView01=0x7f090000;
-        public static final int action_settings=0x7f09000c;
-        public static final int btnSendMessage=0x7f090008;
-        public static final int btnSetCustomMessage=0x7f090003;
+        public static final int action_settings=0x7f09000b;
+        public static final int editCustomMessage=0x7f090007;
         public static final int etMessage1=0x7f090002;
-        public static final int info=0x7f09000b;
-        public static final int lbLine1=0x7f090004;
-        public static final int lbLine2=0x7f090009;
-        public static final int lbResponse=0x7f09000a;
-        public static final int radioButton2=0x7f090006;
-        public static final int radioButton3=0x7f090007;
-        public static final int rbMessage1=0x7f090005;
+        public static final int info=0x7f09000a;
+        public static final int lbLine1=0x7f090003;
+        public static final int lbLine2=0x7f090008;
+        public static final int lbResponse=0x7f090009;
+        public static final int radioButton2=0x7f090005;
+        public static final int radioButton3=0x7f090006;
+        public static final int rbMessage1=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- app/src/main/java/net/dlogic/android/nfc/nfctagemudataexchange/MainActivity.java
+++ app/src/main/java/net/dlogic/android/nfc/nfctagemudataexchange/MainActivity.java
@@ -3,6 +3,7 @@ package net.dlogic.android.nfc.nfctagemudataexchange;
 import android.app.Activity;
 import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Menu;
 import java.io.IOException;
 
@@ -13,7 +14,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentFilter.MalformedMimeTypeException;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -22,16 +25,24 @@ public class MainActivity extends Activity {
     private static PendingIntent mPendingIntent;
     private static IntentFilter[] mFilters;
     private static String[][] mTechLists;
-
+    private EditText inputText;
     private TextView info;
 
+    public NfcA tag;
+    public boolean tag_connected=false;
+    public byte[] previous_received_data;
+    public int message_to_send=1;
+
+    public int global_counter=0;
+
+    public DispParam display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         info = (TextView) findViewById(R.id.info);
-
+        inputText = (EditText) findViewById(R.id.editCustomMessage);
         mAdapter = NfcAdapter.getDefaultAdapter(this);
 
         if (mAdapter == null) {
@@ -56,7 +67,37 @@ public class MainActivity extends Activity {
             resolveIntent(intent);
         }
 
+        CountDownTimer remainingTimeCounter = new CountDownTimer(300000000, 10) {
+
+            public void onTick(long millisUntilFinished) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (tag_connected){
+                            try {
+                                ReadDataEmuCard(display, (byte) 0, (byte) 50, tag);
+                            } catch (IOException e){
+                                tag_connected=false;
+                        //        byte[] new_line = {(byte)'\r', (byte)'\n'};
+                            //    preview(e.getLocalizedMessage());
+                            //    display.display_data = concatenateByteArrays(display.display_data, new_line);
+                                String empty=" ";
+                             //   display.display_data = concatenateByteArrays(display.display_data, e.toString().getBytes());
+                                display.display_data = empty.toString().getBytes();
+                                preview(display.display_data);
+                            }
+                        }
+                    }
+                });
+            }
+
+            public void onFinish() {
+                this.start();
+            }
+        }.start();
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -93,17 +134,80 @@ public class MainActivity extends Activity {
         byte[] received_data;
 
         received_data = tag.transceive(writeStream);
-        disp_data.display_data = concatenateByteArrays(disp_data.display_data, "[".toString().getBytes());
-        disp_data.display_data = concatenateByteArrays(disp_data.display_data, new Integer(received_data.length).toString().getBytes());
-        disp_data.display_data = concatenateByteArrays(disp_data.display_data, "] : ".toString().getBytes());
+    //    disp_data.display_data = concatenateByteArrays(disp_data.display_data, "[".toString().getBytes());
+    //    disp_data.display_data = concatenateByteArrays(disp_data.display_data, new Integer(received_data.length).toString().getBytes());
+    //    disp_data.display_data = concatenateByteArrays(disp_data.display_data, "] : ".toString().getBytes());
 
         if (received_data.length > 1) {
-            disp_data.display_data = concatenateByteArrays(disp_data.display_data, received_data);
+        //    disp_data.display_data = concatenateByteArrays(disp_data.display_data, received_data);
+
+            boolean not_equal_array=false;
+            if (previous_received_data.length <= received_data.length) {
+                for (int f = 0; f < previous_received_data.length; f++) {
+                    Integer j = (int) received_data[f];
+                    Integer k = (int) previous_received_data[f];
+                    if (j!=k){
+                        not_equal_array=true;
+                        break;
+                    }
+                }
+            }
+            if (not_equal_array) {
+
+                disp_data.display_data = received_data;
+         //       disp_data.display_data = concatenateByteArrays(disp_data.display_data, "\r\n".toString().getBytes());
+          //      disp_data.display_data = concatenateByteArrays(disp_data.display_data, previous_received_data);
+                previous_received_data=received_data;
+                Integer i = (int) received_data[0];
+                if (i == 0) {
+                    String iamandroid="";
+                    if (message_to_send==1){
+                        iamandroid="Hey PC where are you?";
+                    }
+                    if (message_to_send==2){
+                        iamandroid="I am Android phone";
+                    }
+                    if (message_to_send==3){
+                        iamandroid=inputText.getText().toString();
+                    }
+                    byte[] tmp=iamandroid.toString().getBytes();
+
+
+            //        tmp = {(byte) 'I', (byte) ' ', (byte) 'a', (byte) 'm', (byte) ' ', (byte) 'a', (byte) 'n', (byte) 'd', (byte) 'r', (byte) 'o', (byte) 'i', (byte) 'd',
+            //                (byte) ' ', (byte) 'p', (byte) 'h', (byte) 'o', (byte) 'n', (byte) 'e'};
+                    previous_received_data = tmp;
+                    WriteDataEmuCard(disp_data, (byte) 0, tmp, tag);
+                }
+                preview(display.display_data);
+            }
         } else {
             Integer i = (int)received_data[0];
-            disp_data.display_data = concatenateByteArrays(disp_data.display_data, i.toString().getBytes());
+            if (i==-31){
+             //   disp_data.display_data = new byte[]{(byte) 'O', (byte) 'K'};
+             //    preview(display.display_data);
+            } else {
+                if (i==-32){
+                    disp_data.display_data = new byte[]{(byte) 'W', (byte) 'r', (byte) 'i', (byte) 't', (byte) 'e', (byte) ' ',(byte) 'f', (byte) 'a', (byte) 'i', (byte) 'l'};
+                    preview(display.display_data);
+                } else {
+                    disp_data.display_data = concatenateByteArrays(disp_data.display_data, i.toString().getBytes());
+                }
+            }
         }
-        disp_data.display_data = concatenateByteArrays(disp_data.display_data, "\r\n".toString().getBytes());
+    //    disp_data.display_data = concatenateByteArrays(disp_data.display_data, "\r\n".toString().getBytes());
+    }
+
+    public void WriteDataEmuCard(DispParam disp_data, byte address ,byte[] writeStream, NfcA tag) throws IOException {
+        byte byWrCmd = (byte)0xEA;
+        byte[] wr_cmd = {byWrCmd, address, (byte) writeStream.length};
+        wr_cmd = concatenateByteArrays(wr_cmd, writeStream);
+        TransceiveEmuCard(disp_data, wr_cmd, tag);
+    }
+
+    public void ReadDataEmuCard(DispParam disp_data, byte address ,byte readLength, NfcA tag) throws IOException {
+        byte byRdCmd = (byte)0xEB;
+        byte[] rd_cmd = {byRdCmd, address, readLength};
+        TransceiveEmuCard(disp_data, rd_cmd, tag);
     }
 
     /**
@@ -118,43 +222,57 @@ public class MainActivity extends Activity {
             if (card_exists) {
 
                 Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-                NfcA tag = NfcA.get(tagFromIntent);
+                tag = NfcA.get(tagFromIntent);
 ;
-                DispParam display = new DispParam();
+                display = new DispParam();
                 preview(display.display_data);
 
                 byte[] new_line = {(byte)'\r', (byte)'\n'};
-
+/*
                 byte byWrCmd = (byte)0xEA;
                 byte byRdCmd = (byte)0xEB;
                 byte byResponseOk = (byte)0xE1;
                 byte byResponseFail = (byte)0xE0;
-                byte[] wr_cmd = {byWrCmd, 0, 60};
+                byte[] wr_cmd = {byWrCmd, 0, 18};
                 byte[] wr_cmdA = {byWrCmd, 0, 60};
                 byte[] wr_cmd2 = {byWrCmd, 16, 20};
                 byte[] rd_cmd = {byRdCmd, 0, 60};
-
+*/
                 int blocks;
 
+                /*
                 for (byte i = 0; i < 60; i++) {
                     byte[] tmp = {(byte)(i + 64)};
                     wr_cmd = concatenateByteArrays(wr_cmd, tmp);
                 }
+                */
 
-                for (byte i = 0; i < 60; i++) {
-                    byte[] tmp = {(byte)60};
-                    wr_cmdA = concatenateByteArrays(wr_cmdA, tmp);
-                }
+            //    byte [] tmp = {(byte)'I',(byte)' ', (byte)'a', (byte)'m', (byte)' ', (byte)'a', (byte)'n', (byte)'d', (byte)'r', (byte)'o', (byte)'i', (byte)'d',
+            //            (byte)' ', (byte)'p', (byte)'h', (byte)'o', (byte)'n', (byte)'e'};
+
+                byte [] tmp = {(byte)'0'};
+
+                previous_received_data = tmp;
+               /*    wr_cmd = concatenateByteArrays(wr_cmd, tmp);
+
+
 
                 for (byte i = 0; i < 20; i++) {
-                    byte[] tmp = {(byte)64};
-                    wr_cmd2 = concatenateByteArrays(wr_cmd2, tmp);
-                }
+                    byte[] tmp1 = {(byte)64};
+                    wr_cmd2 = concatenateByteArrays(wr_cmd2, tmp1);
+                }*/
 
                 try {
 
                     tag.connect();
 
+                    tag_connected=true;
+
+           //         WriteDataEmuCard(display,(byte)0, tmp, tag);
+
+           //         ReadDataEmuCard(display, (byte)0, (byte)33, tag);
+                   /* TransceiveEmuCard(display, wr_cmd, tag);
+                    /*
                     TransceiveEmuCard(display, wr_cmdA, tag);
                     TransceiveEmuCard(display, rd_cmd, tag);
 
@@ -163,14 +281,15 @@ public class MainActivity extends Activity {
 
                     TransceiveEmuCard(display, wr_cmd2, tag);
                     TransceiveEmuCard(display, rd_cmd, tag);
+*/
 
-                    preview(display.display_data);
+              //      preview(display.display_data);
 
                 } catch (IOException e) {
-                    preview(e.getLocalizedMessage());
-                    display.display_data = concatenateByteArrays(display.display_data, new_line);
-                    display.display_data = concatenateByteArrays(display.display_data, e.toString().getBytes());
-                    preview(display.display_data);
+               //     preview(e.getLocalizedMessage());
+                //    display.display_data = concatenateByteArrays(display.display_data, new_line);
+               //     display.display_data = concatenateByteArrays(display.display_data, e.toString().getBytes());
+               //     preview(display.display_data);
                 }
             }
         }
@@ -206,6 +325,27 @@ public class MainActivity extends Activity {
     public void preview(String text) {
 
         info.setText(text);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.rbMessage1:
+                if (checked)
+                    message_to_send=1;
+                    break;
+            case R.id.radioButton2:
+                if (checked)
+                    message_to_send=2;
+                    break;
+            case R.id.radioButton3:
+                if (checked)
+                    message_to_send=3;
+                    break;
+        }
     }
 
 }
